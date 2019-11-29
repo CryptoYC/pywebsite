fe538e80ea29a4ca8f6ad241a8e35a6084afb98d
root <root@evolabwebsite.12vnov4gnesurewbn4bp45oqja.mx.internal.cloudapp.net>
2019-11-29 07:34:19 +0000

    add btc_price&run ip

--- app/data.py
+++ app/data.py
@@ -1,14 +1,15 @@
 import requests
 import json
-
-
+import time
+from bs4 import BeautifulSoup
 def chaininfo():
     url = "https://api.chain.info/v2/entity/list"
     res = requests.get(url)
     js = res.json()
-    if res.status_code == requests.codes.ok:
+    
+    if res.status_code==requests.codes.ok:
         if not js['success']:
-            return '404'
+            return '404'   
         else:
             return js
     else:
@@ -19,20 +20,43 @@ def sncrating():
     url = "https://sncrating.com/api/exchange/index"
     res = requests.get(url)
     js = res.json()
-    if res.status_code == requests.codes.ok:
-        if not js['status'] == 0:
+    if res.status_code==requests.codes.ok:
+        if not js['status'] == 0  :
             return '404'
         else:
             return js
     else:
         return 'error'
 
+    
+
+
 
 def troytrade():
-    url = "https://rdtradeapi.jar.today/t/public/data/blockchainchart/activeaddresses"
-    r = requests.post(url=url)
-    data = r.json()
-    if data["code"] == "200":
-        return data
+    URL = "https://rdtradeapi.jar.today/t/public/data/blockchainchart/activeaddresses"
+    r=requests.post(url=URL)
+    url = ""
+    data=r.json()
+    if data["code"]=="200":
+        lenth = len(data['data']['btc'])
+        price = btcPrice()
+        price_len = len(price)
+        b = [0]*(lenth-price_len)
+        btc_price = b+ price
+        data['btc_price'] = btc_price
+        return json.dumps(data) 
     else:
         return 'error'
+
+def btcPrice():
+    url = "https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20091029&end="
+    res =requests.get(url)
+    list=[]
+    soup = BeautifulSoup(res.text,'html5lib')
+    for i in soup.select('tr.cmc-table-row.sc-1ebpa92-0.kQmhAn'):
+        date = i.select('td')[0].get_text()
+        price = i.select('td')[1].get_text()
+        d = time.strptime(date,'%b %d, %Y')
+        date = time.strftime("%Y-%m-%d %H:%M:%S",d)
+        list.append(float(price.replace(',','')))
+    return list[::-1]
